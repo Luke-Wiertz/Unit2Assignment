c0a485dced35729e485adf6b0e80d1c88998e826
Luke-Wiertz <wiertzema.luke@gmail.com>
2025-02-04 17:57:34 -0600

    AverageValue.java

--- AverageValue.java
+++ AverageValue.java
@@ -42,20 +42,4 @@ public class AverageValue {
     System.out.println("No values were entered.");
     }
     }
-
-    // Main method
-public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    AverageValue avg = new AverageValue();
-    String more = "y";
-
-    while (more.equalsIgnoreCase("y")) {
-    avg.accumulate(scanner);
-    System.out.print("more? (y/n): ");
-    more = scanner.next();
-    }
-
-    avg.printAverage();
-    scanner.close();
-    }
 }
